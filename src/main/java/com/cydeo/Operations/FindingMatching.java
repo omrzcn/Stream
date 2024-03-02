8d5b5601940bd6e378bf45b255560a77104ef535
omrzcn <omrzcnn@gmail.com>
2024-03-02 16:08:05 +0300

    more methods from stream

--- src/main/java/com/cydeo/Operations/FindingMatching.java
+++ src/main/java/com/cydeo/Operations/FindingMatching.java
@@ -1,5 +1,32 @@
 package com.cydeo.Operations;
 
+import com.cydeo.Task.DishData;
+
 public class FindingMatching {
-    //
+    public static void main(String[] args) {
+
+        System.out.println("================ALL MATCH=====================");
+
+        boolean isHealthy = DishData.getAll().stream().allMatch(dish -> dish.getCalories()<1000);
+        System.out.println(isHealthy); // all of the dishes are low than 1000
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
